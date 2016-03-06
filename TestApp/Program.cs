199566599bccbadee81eca7caa141989d28546f1
Sean Feldman <feldman.sean@gmail.com>
2016-03-06 16:38:01 -0700

    adding gitVersionConfig.yaml

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyName = assembly.GetName().Name;
+            var gitVersionInformationType = assembly.GetType(assemblyName + ".GitVersionInformation");
+            var fields = gitVersionInformationType.GetFields();
+
+            foreach (var field in fields)
+            {
+                Trace.WriteLine(string.Format("{0}: {1}", field.Name, field.GetValue(null)));
+            }
+
+            var versionField = gitVersionInformationType.GetField("Minor");
+            Trace.WriteLine(versionField.GetValue(null));
+
+            Console.ReadLine();
         }
     }
 }
